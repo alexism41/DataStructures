cf1ab9608f6a4a70686d32f053634d8c43e945f3
Alexis <muroa1@uci.edu>
2021-07-08 18:20:36 -0700

    completed remaining DoublyLinkedListTest test cases

--- tests/LinkedLists/DoublyLinkedListTest.java
+++ tests/LinkedLists/DoublyLinkedListTest.java
@@ -30,21 +30,72 @@ class DoublyLinkedListTest {
     @Test
     @DisplayName("Should return true when empty")
     void isEmpty() {
+        assertTrue(list.isEmpty());
+        list.insert(1);
+        assertFalse(list.isEmpty());
+        list.insert(2);
+        assertFalse(list.isEmpty());
+        list.remove();
+        assertFalse(list.isEmpty());
+        list.remove();
+        assertTrue(list.isEmpty());
     }
 
     @Test
     @DisplayName("Should return data from the front of the list")
     void getHead() {
+        assertThrows(NullPointerException.class, () -> list.getHead());
+        list.insert(0,1);
+        assertEquals(1, list.getHead());
+        list.insert(2);
+        list.insert(3);
+        assertEquals(1, list.getHead());
+        list.remove(0);
+        assertEquals(2, list.getHead());
+        list.remove(0);
+        list.remove(0);
+        assertThrows(NullPointerException.class, () -> list.getHead());
     }
 
     @Test
     @DisplayName("Should return data from the last node in the list")
     void getTail() {
+        assertThrows(NullPointerException.class, () -> list.getTail());
+        list.insert(1);
+        assertEquals(1, list.getTail());
+        list.remove();
+        assertThrows(NullPointerException.class, () -> list.getTail());
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+        assertEquals(6, list.getTail());
+        list.remove();
+        assertEquals(5, list.getTail());
+        list.remove(2);
+        assertEquals(5, list.getTail());
     }
 
     @Test
     @DisplayName("Should return data at given index")
     void getAt() {
+        assertThrows(IndexOutOfBoundsException.class, () -> list.getAt(0));
+        list.insert(0, 1);
+        list.insert(1, 2);
+        list.insert(2, 3);
+        list.insert(3, 4);
+        list.insert(5);
+        list.insert(6);
+        list.insert(6, 7);
+        assertThrows(IndexOutOfBoundsException.class, () -> list.getAt(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.getAt(7));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.getAt(list.size()));
+        assertEquals(2, list.getAt(1));
+        assertEquals(3, list.remove(2));
+        assertEquals(4, list.getAt(2));
+        list.remove();
+        assertEquals(6, list.getAt(list.size()-1));
     }
 
     @Test
